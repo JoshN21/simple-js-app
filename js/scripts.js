d87b02688b706f5715ad7bd13424b95f818f73f7
JoshN21 <joshn54@gmail.com>
2022-08-29 13:44:50 -0700

    Update scripts.js
    Created basic array with three pokemon objects

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+  {name: 'Bulbasaur', height: 2, types: ['grass', 'poison']},
+  {name: 'Ivysaur', height: 3, types: ['grass', 'poison']},
+  {name: 'Venusaur', height: 6, types: ['grass', 'poison']}
+];
