f79d92aa9463294f66aedea0ee5195a5a5f0067d
JoshN21 <joshn54@gmail.com>
2022-09-20 17:49:44 -0700

    Update scripts.js
    attempt to use IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,39 @@
-let pokemonList = [
-  {name: 'Bulbasaur', height: 2, types: ['grass', 'poison']},
-  {name: 'Ivysaur', height: 3, types: ['grass', 'poison']},
-  {name: 'Venusaur', height: 6, types: ['grass', 'poison']}
-];
+//pokemonList in IIFE
+let pokemonRepository = (function () {
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height >= 5) {
-      document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + '- Wow, that\'s big!' + '<br>');
-  } else {
-      document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' +'<br>');
-  }
-}
+  let pokemonList = [
+    {name: 'Bulbasaur', height: 2, types: ['grass', 'poison']},
+    {name: 'Ivysaur', height: 3, types: ['grass', 'poison']},
+    {name: 'Venusaur', height: 6, types: ['grass', 'poison']}
+  ];
 
+  function getAll() {
+    return pokemonList;
+  };
 
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  };
 
+  return {
+    getAll: getAll,
+    add: add
+  };
+})();
 
+//forEach loop iterating over pokemonList, Function composition
+function getHeightDescription(height) {
+  return height + 'ft.';
+};
 
+function getTypesDescription(types) {
+  return 'types: ' + types;
+};
 
+function getPokemonDescription(pokemon) {
+  let heightDescription = getHeightDescription(pokemon.height);
+  let typesDescription = getTypesDescription(pokemon.types);
+  document.write(pokemon.name + ', ' + heightDescription + ', ' + typesDescription + '<br>');
+};
 
-
-
-
-
-
-
-
-// CONDITIONAL EXAMPLE
-
-// let day = 'Saturday';
-// if (day = 'Saturday' || day = 'Sunday') {
-//   console.log("It's the weekend!");
-// } else {
-//   console.log("It's not the weekend.")
-// }
-
-
-//FOR LOOP EXAMPLE - INITIALIZATION, CONDITION, ACTION
-
-// let person = [
-//   {name: 'brian', age: 16},
-//   {name: 'lisa', age: 10},
-//   {name: 'cole', age: 25}
-// ];
-
-// for (let i = 0; i < person.length; i++) {
-//     if (person[i].age < 20 && person[i].age > 12) {
-//         console.log(person[i].name + ' is a teenager');
-//     } else if (person[i].age < 13) {
-//         console.log(person[i].name + ' is a child');
-//     } else {
-//         console.log(person[i].name + ' is an adult');
-//     }
-// }
-
-
-// WHILE LOOP EXAMPLES
-
-// let fruits = ['apple', 'banana', 'orange', 'grape']
-// let text = '';
-// let i = 0;
-//
-// while (fruits[i]) {
-//   text = text + '' + fruits[i];
-//   i++;
-// }
-//
-// console.log(text);
-
-
-// let i = 1;
-//
-// do {
-//  console.log(i);
-// i++
-// }
-// while ( i < 5)
+pokemonList.forEach(getPokemonDescription);
